a5027b03c904c4687e94712c1271eab212ba7995
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-15 11:01:48 +0800

    Set up GET request for each product type

--- students/ting/stylish/index.js
+++ students/ting/stylish/index.js
@@ -1,55 +1,72 @@
+/* ==========================================================================
+   Week 1 Part 3
+   ========================================================================== */
+// Destination of API
+const API = 'https://api.appworks-school.tw/api/1.0'
 
-// Setting up Product page to pull product info (ALL) from API
+// Set up GET function
+function ajax(src, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // This will run when the request is successful
+            console.log('success!');
+            const list = JSON.parse(xhr.responseText);
+            console.log(list)
+            callback(list);
+        } else {
+            // This will run when it's not
+            console.log('The request failed!');
+        };
+    }
+    xhr.open('GET', src , true);
+    xhr.send();
+};
+
+// Set up render function
 const container = document.getElementById('main-content')
 
-const xhr = new XMLHttpRequest();
-xhr.onload = function () {
-    const list = JSON.parse(xhr.responseText).data;
-    console.log(list)
+function render(list) {
+    // Make homepage's Products disappear
+    container.removeChild(container.firstElementChild);
 
-    if (xhr.status >= 200 && xhr.status < 300) {
-        // This will run when the request is successful
-        console.log('success!');
+    const products = document.createElement('div')
+    products.setAttribute('class', 'products')
+
+    const productData = list.data;
+    for (let i = 0; i < productData.length; i++) {
+        // Individual product containter
+        const product = document.createElement('div')
+        product.setAttribute('class', 'product')
+
+        // Product Main Image
+        const productImg = document.createElement('img')
+        productImg.src = productData[i].main_image
         
-        for (let i = 0; i < list.length; i++) {
-                // Individual product containter
-                const product = document.createElement('div')
-                product.setAttribute('class', 'product')
-
-                // Product Main Image
-                const productImg = document.createElement('img')
-                productImg.src = list[i].main_image
-                
-                // Product Color Types
-                const productColor = document.createElement('div')
-                productColor.setAttribute('class', 'color')
-                const colorBox = list[i].colors
-                for (let a = 0; a < colorBox.length; a++) {
-                    productColor.style.backgroundColor = `#`+colorBox[a].code
-                }
-
-                // Product Name
-                const productName = document.createElement('div')
-                productName.setAttribute('class', 'name')
-                productName.textContent = list[i].title
-
-                // Product Price
-                const productPrice = document.createElement('div')
-                productPrice.setAttribute('class', 'price')
-                productPrice.textContent = 'TWD.' + list[i].price
-
-                container.appendChild(product)
-                product.appendChild(productImg)
-                product.appendChild(productColor)
-                product.appendChild(productName)
-                product.appendChild(productPrice)
-
+        // Product Color Types
+        const productColor = document.createElement('div')
+        productColor.setAttribute('class', 'color')
+        const colorBox = productData[i].colors
+        for (let a = 0; a < colorBox.length; a++) {
+            productColor.style.backgroundColor = `#`+colorBox[a].code
         }
 
-    } else {
-		// This will run when it's not
-		console.log('The request failed!');
-	}
+        // Product Name
+        const productName = document.createElement('div')
+        productName.setAttribute('class', 'name')
+        productName.textContent = productData[i].title
+
+        // Product Price
+        const productPrice = document.createElement('div')
+        productPrice.setAttribute('class', 'price')
+        productPrice.textContent = 'TWD.' + productData[i].price
+
+        container.appendChild(products)
+        products.appendChild(product)
+        product.appendChild(productImg)
+        product.appendChild(productColor)
+        product.appendChild(productName)
+        product.appendChild(productPrice)
+    }
+
 };
-xhr.open('GET', 'https://api.appworks-school.tw/api/1.0/products/all', true);
-xhr.send();
