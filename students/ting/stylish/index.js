394a5c0a4d9d6a6c9698cdb2c4e23dbdd01136f1
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-17 18:32:32 +0800

    Add onclick function for when user clicks on banner's dots

--- students/ting/stylish/index.js
+++ students/ting/stylish/index.js
@@ -256,9 +256,7 @@ function renderMarketing(list) {
         marketingContainer.appendChild(marketingImg)
         marketingImg.appendChild(marketingText)
     }    
-    
     showSlides();
-    
 }
 
 // Render marketing campaign banner
@@ -283,7 +281,21 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
-  }
+}
+
+// Renders Banner on click
+function oneShow(n) {
+    const slides = document.getElementsByClassName("main-banner");
+    const dots = document.getElementsByClassName("dot");
+    //  resets all banner display to none
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n].style.display = 'block'
+}
 
 
       
