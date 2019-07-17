c30b6632d3f2518da9fb0f13d38740eede606a1b
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-17 17:52:26 +0800

    Move showSlides function to global scope

--- students/ting/stylish/index.js
+++ students/ting/stylish/index.js
@@ -257,30 +257,33 @@ function renderMarketing(list) {
         marketingImg.appendChild(marketingText)
     }    
     
-    // Slideshow function
-    let slideIndex = 0;
     showSlides();
-    function showSlides() {
-      const slides = document.getElementsByClassName("main-banner");
-      const dots = document.getElementsByClassName("dot");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active";
-      setTimeout(showSlides, 3000); // Change image every 2 seconds
-    }
+    
 }
 
 // Render marketing campaign banner
 productLoad(`${APImarketing}`,renderMarketing);
 
+// Slideshow function
+let slideIndex = 0;
+function showSlides() {
+    const slides = document.getElementsByClassName("main-banner");
+    const dots = document.getElementsByClassName("dot");
+    // resets all dots and banner to none
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} // infinite loop
+    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
 
 
       
