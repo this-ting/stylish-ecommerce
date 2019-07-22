a36087f147b7b62a3f4e7fdcf265655c4620e7ab
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-22 11:32:16 +0800

    Set up minimum framework to find 'cart' in local storage

--- students/ting/stylish/script/lib.js
+++ students/ting/stylish/script/lib.js
@@ -25,3 +25,88 @@ function callAPI(src, callback) {
     xhr.open('GET', src);
     xhr.send();
 };
+
+// Set up cart local storage layout
+
+let cartItem = {
+    id: 123456,
+    name: "洋裝",
+    price: 299,
+    color: {
+        name: "blue",
+        code: "FFFFFF"
+    },
+    size: "S",
+    qty: 1
+    }
+
+// let cartList = {`${cartItem}`}; // an array of objects for each product bought
+  /* [
+    {
+      "id": [Product ID],
+      "name": [Product Name],
+      "price": [Product Unit Price],
+      "color": {
+        "name": [Product Variant Color Name],
+        "code": [Product Variant Color HexCode]
+      },
+      "size": [Product Variant Size],
+      "qty": [Quantity]
+    }
+  ] */
+
+
+  let cartDetails = {
+    "prime": "", 
+    "order": {
+      "shipping": "delivery",
+      "payment": "credit_card",
+      "subtotal": "", 
+      "freight": "", 
+      "total": "",
+      "recipient": {
+        "name": "", 
+        "phone": "", 
+        "email": "",
+        "address": "", 
+        "time": "", 
+      },
+      "list": [
+        {
+        id: 123456,
+        main_image: "https://api.appworks-school.tw/assets/201807242222/main.jpg",
+        name: "洋裝",
+        price: 299,
+        color: {
+            name: "blue",
+            code: "FFFFFF"
+            },
+        size: "S",
+        qty: 1
+        },
+        {
+        id: 123456,
+        main_image: "https://api.appworks-school.tw/assets/201807242222/main.jpg",
+        name: "洋裝",
+        price: 299,
+        color: {
+            name: "blue",
+            code: "FFFFFF"
+            },
+        size: "S",
+        qty: 1
+        }
+    ]
+    }
+  }
+
+// Check local storage for 'cart'
+if (localStorage.getItem("cart") === null ) {
+    console.log('there is no cart') 
+    localStorage.setItem("cart", `${JSON.stringify(cartDetails)}`)
+
+} else {
+    localStorage.getItem("cart")
+    console.log('there is a cart')
+}
+
