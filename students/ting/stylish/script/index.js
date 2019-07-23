c0c46d6711a7b657061180102f7ffc328c34d97c
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-23 14:17:50 +0800

    Clean up code - add semicolons

--- students/ting/stylish/script/index.js
+++ students/ting/stylish/script/index.js
@@ -10,58 +10,58 @@ let category = 'all';
 
 // Render homepage products on load
 if ( window.location.search === '?tag=women') {
-    callAPI(`${APIproducts}women`,render)
+    callAPI(`${APIproducts}women`,render);
 } else if ( window.location.search === '?tag=men') {
-    callAPI(`${APIproducts}men`,render)
+    callAPI(`${APIproducts}men`,render);
 } else if ( window.location.search === "?tag=accessories") {
-    callAPI(`${APIproducts}accessories`,render)
+    callAPI(`${APIproducts}accessories`,render);
 } else {
-    callAPI(`${APIproducts}all`,render)
+    callAPI(`${APIproducts}all`,render);
 }
 
 // Set up products render function
-const container = document.getElementById('main-content')
+const container = document.getElementById('main-content');
 
 function render(list) {
     // Make homepage's Products disappear
     container.removeChild(container.firstElementChild);
 
-    const products = document.createElement('div')
-    products.setAttribute('class', 'products')
-    container.appendChild(products)
+    const products = document.createElement('div');
+    products.setAttribute('class', 'products');
+    container.appendChild(products);
 
     const productData = list.data;
     for (let i = 0; i < productData.length; i++) {
         // Individual product containter
-        const product = document.createElement('a')
-        product.setAttribute('class', 'product')
-        product.setAttribute('href', `product.html?id=${productData[i].id}`)
-        products.appendChild(product)
+        const product = document.createElement('a');
+        product.setAttribute('class', 'product');
+        product.setAttribute('href', `product.html?id=${productData[i].id}`);
+        products.appendChild(product);
 
         // Product Main Image
-        const productImg = document.createElement('img')
-        productImg.src = productData[i].main_image
-        product.appendChild(productImg)
+        const productImg = document.createElement('img');
+        productImg.src = productData[i].main_image;
+        product.appendChild(productImg);
         
         // Product Color Types
-        const colorBox = productData[i].colors
+        const colorBox = productData[i].colors;
         for (let a = 0; a < colorBox.length; a++) {
-            const productColor = document.createElement('div')
-            productColor.setAttribute('class', 'color')
-            productColor.style.backgroundColor = `#`+colorBox[a].code
-            product.appendChild(productColor)
-        }
+            const productColor = document.createElement('div');
+            productColor.setAttribute('class', 'color');
+            productColor.style.backgroundColor = `#`+colorBox[a].code;
+            product.appendChild(productColor);
+        };
         // Product Name
-        const productName = document.createElement('div')
-        productName.setAttribute('class', 'name')
-        productName.textContent = productData[i].title
-        product.appendChild(productName)
+        const productName = document.createElement('div');
+        productName.setAttribute('class', 'name');
+        productName.textContent = productData[i].title;
+        product.appendChild(productName);
 
         // Product Price
-        const productPrice = document.createElement('div')
-        productPrice.setAttribute('class', 'price')
-        productPrice.textContent = 'TWD.' + productData[i].price
-        product.appendChild(productPrice)
+        const productPrice = document.createElement('div');
+        productPrice.setAttribute('class', 'price');
+        productPrice.textContent = 'TWD.' + productData[i].price;
+        product.appendChild(productPrice);
 
     };
 };
@@ -75,12 +75,9 @@ const searchInput = document.getElementById('nav-search');
 function searchProduct(src, callback) {
     xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-            console.log('Search Success!')
-            console.log(`You searched up '${searchInput.value}'.`)
+            console.log('Search Success!');
             const search = JSON.parse(xhr.responseText);
-            console.log(search)
             callback(search);
-
         } else {
             console.log('The request has failed');
         };
@@ -92,17 +89,15 @@ function searchProduct(src, callback) {
 // Function will check if there is such product then render appropriate output
 function searchRender(search) {
     if (search.error ==  "Wrong Request") {
-        // Show "Type search" message
-        console.log('Please enter into the search bar')
         // Make Existing Products disappear
         container.removeChild(container.firstElementChild);
     
         // No Product Message
-        const noProduct = document.createElement('div')
-        noProduct.setAttribute('class', 'noProduct')
-        noProduct.textContent = '請輸入搜尋的產品哦'
+        const noProduct = document.createElement('div');
+        noProduct.setAttribute('class', 'noProduct');
+        noProduct.textContent = '請輸入搜尋的產品哦';
         
-        container.appendChild(noProduct)
+        container.appendChild(noProduct);
 
     } else if (search.data.length === 0) {
         // Show "No product available" message
@@ -112,16 +107,15 @@ function searchRender(search) {
         container.removeChild(container.firstElementChild);
         
         // No Product Message
-        const noProduct = document.createElement('div')
-        noProduct.setAttribute('class', 'noProduct')
-        noProduct.textContent = 'Error 404 沒有所搜尋的產品哦'
+        const noProduct = document.createElement('div');
+        noProduct.setAttribute('class', 'noProduct');
+        noProduct.textContent = 'Error 404 沒有所搜尋的產品哦';
         
-        container.appendChild(noProduct)
+        container.appendChild(noProduct);
 
     } else {
         // Make homepage's Products disappear
-        console.log('Here is your product!');
-        render(search)
+        render(search);
     }
     // remove event listener
     window.removeEventListener('scroll', infiniteScroll);
@@ -130,28 +124,26 @@ function searchRender(search) {
 // Mobile search text input pop up
 function showMobileSearch() {
     // Remove top header
-    console.log("i am clicked")
-    const topHeader = document.getElementById('top-header')
+    const topHeader = document.getElementById('top-header');
     
-    topHeader.removeChild(topHeader.firstElementChild)
+    topHeader.removeChild(topHeader.firstElementChild);
     topHeader.removeChild(topHeader.lastElementChild);
 
     // Add in text input
-    const searchFormMobile = document.createElement('form')
-    const searchInputMobile = document.createElement('input')
-    searchInputMobile.setAttribute('class', 'nav-mobile-search')
-    searchInputMobile.setAttribute('type', 'text')
+    const searchFormMobile = document.createElement('form');
+    const searchInputMobile = document.createElement('input');
+    searchInputMobile.setAttribute('class', 'nav-mobile-search');
+    searchInputMobile.setAttribute('type', 'text');
 
-    const searchSubmitMobile = document.createElement('button')
-    searchSubmitMobile.style.visibility = 'hidden'
+    const searchSubmitMobile = document.createElement('button');
+    searchSubmitMobile.style.visibility = 'hidden';
 
-    topHeader.appendChild(searchFormMobile)
-    searchFormMobile.appendChild(searchInputMobile)
-    searchFormMobile.appendChild(searchSubmitMobile)
+    topHeader.appendChild(searchFormMobile);
+    searchFormMobile.appendChild(searchInputMobile);
+    searchFormMobile.appendChild(searchSubmitMobile);
 
     document.querySelector("form").addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log('hi')
         searchProduct(`${APIsearch}${searchInputMobile.value}`,searchRender);
     });
 
@@ -166,18 +158,15 @@ let pageLoading=false; // to prevent renderScroll from running multiple times wh
 const infiniteScroll = function () {
     // set event to fire when scrolling reaches end of container
     if (window.innerHeight = container.getBoundingClientRect().bottom) {
-        this.console.log('You have reached the scroll trigger point');
-    
         // Function to check if there is a next page and produce URL to API
         let APIpage = `${APIproducts}${category}?paging=`;
-        nextPg = `${APIpage}${haveNext}`
+        nextPg = `${APIpage}${haveNext}`;
         if (haveNext !== undefined && pageLoading === false) {
             pageLoading = true;
             callAPI(nextPg, renderScroll);
         } else {
             return;
         };
-
     };
 }
 window.addEventListener('scroll', infiniteScroll);
@@ -185,40 +174,39 @@ window.addEventListener('scroll', infiniteScroll);
 // Infinite scroll function to add products from the next page 
 function renderScroll(list) {
 
-    const products = document.querySelector(".products")
-
+    const products = document.querySelector(".products");
     const productData = list.data;
     for (let i = 0; i < productData.length; i++) {
         // Individual product containter
-        const product = document.createElement('a')
-        product.setAttribute('class', 'product')
-        product.setAttribute('href', `product.html?id=${productData[i].id}`)
-        products.appendChild(product)
+        const product = document.createElement('a');
+        product.setAttribute('class', 'product');
+        product.setAttribute('href', `product.html?id=${productData[i].id}`);
+        products.appendChild(product);
 
         // Product Main Image
-        const productImg = document.createElement('img')
-        productImg.src = productData[i].main_image
-        product.appendChild(productImg)
+        const productImg = document.createElement('img');
+        productImg.src = productData[i].main_image;
+        product.appendChild(productImg);
         
         // Product Color Types
-        const colorBox = productData[i].colors
+        const colorBox = productData[i].colors;
         for (let a = 0; a < colorBox.length; a++) {
-            const productColor = document.createElement('div')
-            productColor.setAttribute('class', 'color')
-            productColor.style.backgroundColor = `#`+colorBox[a].code
-            product.appendChild(productColor)
+            const productColor = document.createElement('div');
+            productColor.setAttribute('class', 'color');
+            productColor.style.backgroundColor = `#`+colorBox[a].code;
+            product.appendChild(productColor);
         }
         // Product Name
-        const productName = document.createElement('div')
-        productName.setAttribute('class', 'name')
-        productName.textContent = productData[i].title
-        product.appendChild(productName)
+        const productName = document.createElement('div');
+        productName.setAttribute('class', 'name');
+        productName.textContent = productData[i].title;
+        product.appendChild(productName);
 
         // Product Price
-        const productPrice = document.createElement('div')
-        productPrice.setAttribute('class', 'price')
-        productPrice.textContent = 'TWD.' + productData[i].price
-        product.appendChild(productPrice)
+        const productPrice = document.createElement('div');
+        productPrice.setAttribute('class', 'price');
+        productPrice.textContent = 'TWD.' + productData[i].price;
+        product.appendChild(productPrice);
     }
     pageLoading = false; /// to prevent renderScroll from running multiple times when scroll event triggered
 };
@@ -229,21 +217,21 @@ const marketingContainer = document.getElementById('marketing-container');
 
 function renderMarketing(list) {
     // set up for each function to create 3 marketing banners
-    const productData = list.data
+    const productData = list.data;
    
     for (let i = 0; i < productData.length; i++) {
         // Marketing Background Image
-        const marketingImg = document.createElement('div')
-        marketingImg.setAttribute('class', 'main-banner fade')
-        marketingImg.style.backgroundImage = `url(${APIasset}${productData[i].picture})`
+        const marketingImg = document.createElement('div');
+        marketingImg.setAttribute('class', 'main-banner fade');
+        marketingImg.style.backgroundImage = `url(${APIasset}${productData[i].picture})`;
         
         // Marketing Background Text
-        const marketingText = document.createElement('div')
-        marketingText.setAttribute('class', 'banner-text')
-        marketingText.innerText = productData[i].story
+        const marketingText = document.createElement('div');
+        marketingText.setAttribute('class', 'banner-text');
+        marketingText.innerText = productData[i].story;
 
-        marketingContainer.appendChild(marketingImg)
-        marketingImg.appendChild(marketingText)
+        marketingContainer.appendChild(marketingImg);
+        marketingImg.appendChild(marketingText);
     }    
     showSlides();
 }
@@ -265,7 +253,7 @@ function showSlides() {
     }
 
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} // infinite loop
+    if (slideIndex > slides.length) {slideIndex = 1}; // infinite loop
     
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
@@ -283,29 +271,5 @@ function oneShow(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[n].style.display = 'block'
+    slides[n].style.display = 'block';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
