dd49d49ae55e8777f24e2d9657c1278674929c08
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-23 14:19:54 +0800

    Clean up code - move functions to top && add const variables

--- students/ting/stylish/script/index.js
+++ students/ting/stylish/script/index.js
@@ -2,22 +2,13 @@
    Render Home Page & Category Page
    ========================================================================== */
 // Variables
+const API = `https://api.appworks-school.tw/api/1.0`;
+const APIproducts = `${API}/products/`;
 const APIsearch = `${APIproducts}search?keyword=`;
 const APImarketing = `${API}/marketing/campaigns`;
 const APIasset = `https://api.appworks-school.tw`;
 let haveNext; // for product paging
 let category = 'all';
-
-// Render homepage products on load
-if ( window.location.search === '?tag=women') {
-    callAPI(`${APIproducts}women`,render);
-} else if ( window.location.search === '?tag=men') {
-    callAPI(`${APIproducts}men`,render);
-} else if ( window.location.search === "?tag=accessories") {
-    callAPI(`${APIproducts}accessories`,render);
-} else {
-    callAPI(`${APIproducts}all`,render);
-}
 
 // Set up products render function
 const container = document.getElementById('main-content');
@@ -66,6 +57,16 @@ function render(list) {
     };
 };
 
+// Render homepage products on load
+if ( window.location.search === '?tag=women') {
+    callAPI(`${APIproducts}women`,render);
+} else if ( window.location.search === '?tag=men') {
+    callAPI(`${APIproducts}men`,render);
+} else if ( window.location.search === "?tag=accessories") {
+    callAPI(`${APIproducts}accessories`,render);
+} else {
+    callAPI(`${APIproducts}all`,render);
+};
 
 /* ==========================================================================
    Search Function
