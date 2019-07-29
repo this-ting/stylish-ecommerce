940545f0d391a5faa81d2d101604b83a644fee5d
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-29 15:52:21 +0800

    Add in console.log to test FB API response

--- students/ting/stylish/script/user.js
+++ students/ting/stylish/script/user.js
@@ -13,12 +13,21 @@
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
+      console.log(response)
+
+
+
+
+
     } else {
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     }
   }
+
+
+
 
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
@@ -70,8 +79,12 @@
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        
+            console.log(response)
     });
-  }
+  };
+
+
