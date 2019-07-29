858243b73dffcc3f4635acf0ad4db9a46bddc2d4
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-29 11:42:08 +0800

    Set up thank you page to show order number from query string

---
+++ students/ting/stylish/script/thankyou.js
@@ -0,0 +1,8 @@
+/* ==========================================================================
+   Thank You
+   ========================================================================== */
+// Get order number from location.search query string
+let orderNo = location.search.substring(location.search.indexOf('?') + 1).split('&')[0].split('=')[1]
+
+const orderConfirm = document.querySelector('.order');
+orderConfirm.innerText = `您的訂單編號為 ${orderNo}`;
