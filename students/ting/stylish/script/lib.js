d54940032a10a65c58e7247f7270d410513e0afa
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-30 16:33:48 +0800

    Move FB SDK initilization back to user.js

--- students/ting/stylish/script/lib.js
+++ students/ting/stylish/script/lib.js
@@ -69,26 +69,5 @@ if (localStorage.getItem("cart") === null ) {
     setCartQty(); // call setCartQty function
 };
 
-/* ==========================================================================
- Initialize Facebook JavaScript SDK
- ========================================================================== */
-  // Load the SDK asynchronously
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function (response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  };
+
   
--- students/ting/stylish/script/user.js
+++ students/ting/stylish/script/user.js
@@ -84,7 +84,9 @@ function statusChangeCallback(response) {
   };
 }; // end of function statusChangeCallback(response)
 
-
+/* ==========================================================================
+ Initialize Facebook JavaScript SDK
+ ========================================================================== */
 window.fbAsyncInit = function () {
   FB.init({
     appId: '450421272462212',
@@ -107,4 +109,24 @@ window.fbAsyncInit = function () {
   // These three cases are handled in the callback function.
 
   checkLoginState();
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function (response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
 };
