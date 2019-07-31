5ea7ecaca4b16fbc0ad51574651deaf1e09e32a5
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-31 15:51:14 +0800

    Clean up code

--- students/ting/stylish/script/lib.js
+++ students/ting/stylish/script/lib.js
@@ -7,21 +7,21 @@ const APIproducts = `${API}/products/`;
 
 // Set up GET function for API
 function callAPI(src, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            // This will run when the request is successful
-            console.log('API success!');
-            const list = JSON.parse(xhr.responseText);
-            callback(list);
-        } else {
-            // This will run when it's not
-            console.log('The request failed!');
-        };
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // This will run when the request is successful
+      console.log("API success!");
+      const list = JSON.parse(xhr.responseText);
+      callback(list);
+    } else {
+      // This will run when it's not
+      console.log("The request failed!");
     }
-    xhr.open('GET', src);
-    xhr.send();
-};
+  };
+  xhr.open("GET", src);
+  xhr.send();
+}
 
 /* ==========================================================================
    Shopping cart to local storage
@@ -33,37 +33,37 @@ function callAPI(src, callback) {
 */
 
 // Set up cart local storage layout
-let cartDetails = {
-    "prime": "", 
-    "order": {
-        "shipping": "delivery",
-        "payment": "credit_card",
-        "subtotal": "", 
-        "freight": "", 
-        "total": "",
-        "recipient": {
-            "name": "", 
-            "phone": "", 
-            "email": "",
-            "address": "", 
-            "time": "", 
-        },
-        "list": []
-    }
+const cartDetails = {
+  prime: "",
+  order: {
+    shipping: "delivery",
+    payment: "credit_card",
+    subtotal: "",
+    freight: "",
+    total: "",
+    recipient: {
+      name: "",
+      phone: "",
+      email: "",
+      address: "",
+      time: ""
+    },
+    list: []
+  }
 };
 
-//Set up cart quantity update function
+// Set up cart quantity update function
 function setCartQty() {
-    let cartQty = JSON.parse(localStorage.getItem("cart")).order.list;
-    document.querySelector(".cart-mobile-qty").innerHTML = cartQty.length;
-    document.querySelector(".cart-icon-qty").innerHTML = cartQty.length;
-};
+  const cartQty = JSON.parse(localStorage.getItem("cart")).order.list;
+  document.querySelector(".cart-mobile-qty").innerHTML = cartQty.length;
+  document.querySelector(".cart-icon-qty").innerHTML = cartQty.length;
+}
 
 // Check local storage for 'cart'
-if (localStorage.getItem("cart") === null ) {
-    localStorage.setItem("cart", `${JSON.stringify(cartDetails)}`);
-    setCartQty(); // call setCartQty function
+if (localStorage.getItem("cart") === null) {
+  localStorage.setItem("cart", `${JSON.stringify(cartDetails)}`);
+  setCartQty(); // call setCartQty function
 } else {
-    localStorage.getItem("cart");
-    setCartQty(); // call setCartQty function
-};
+  localStorage.getItem("cart");
+  setCartQty(); // call setCartQty function
+}
